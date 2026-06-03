e19c6d82b5d4b0bf7f40e8742b1e59bd7b9a69ea
nickolas <martinsnickolas669@gmail.com>
2026-06-03 05:29:14 -0300

    linkar a pagina brasil no botão

--- main.js
+++ main.js
@@ -5,17 +5,12 @@ gsap.set("#main-svg", { autoAlpha: 1, xPercent: 0, z: 0.1 });
 
 gsap.set("#texto-lateral", { x: 50 }); // Posiciona o texto lateral fora da tela para a direita
 
+gsap.set("#modal-parabens-container", { autoAlpha: 0 }); // NÃO TIRA ESTE CARALHO AQUI
+
+
 const tl = gsap.timeline();
 
-const pecasDoMapa = document.querySelectorAll('#main-svg svg path');
 
-pecasDoMapa.forEach(peca => {
-  peca.addEventListener('click', () => {
-
-    window.location.href = "pagina/das/passivas.html"; 
-    
-  });
-});
 
 
 tl.to("#logo", {
@@ -52,4 +47,76 @@ tl.to("#logo", {
   onComplete: () => {
     document.body.style.overflow = "auto";
   }
+});
+
+const pecasDoMapa = document.querySelectorAll('#main-svg svg path'); 
+
+pecasDoMapa.forEach(peca => {
+  peca.addEventListener('click', () => {
+    
+    // Trava os cliques na tela para o usuário não quebrar a animação
+    document.body.style.pointerEvents = "none";
+
+    const tlClick = gsap.timeline();
+
+    // 1. Faz a imagem de textura desvanecer (sumir) suavemente
+    tlClick.to("#fundo-png", { 
+      autoAlpha: 0, 
+      duration: 1 
+    }, "start") // A tag "start" sincroniza tudo!
+
+    // 2. Muda a cor de fundo do contêiner principal para bordô
+    .to("#main-content", { 
+      backgroundColor: "#72171B", 
+      duration: 1 
+    }, "start")
+
+    // 3A. Mapa voa para a direita e some
+    .to("#main-svg", {
+      x: "20vw",       
+      scale: 0,        
+      autoAlpha: 0,    
+      duration: 1,
+      ease: "power3.inOut"
+    }, "start")
+
+    // 3B. Texto voa para a esquerda e some
+    .to("#texto-lateral", {
+      x: "-20vw",      
+      scale: 0,
+      autoAlpha: 0,
+      duration: 1,
+      ease: "power3.inOut"
+    }, "start")
+
+    // 4. Torna o novo contêiner de parabéns visível
+    .set("#modal-parabens-container", { autoAlpha: 1 })
+
+    // 5. Primeira expansão da bolinha (Altura)
+    .to("#tela-expansiva", {
+      height: 454,
+      borderRadius: 50,
+      duration: 0.6,
+      ease: "power2.inOut"
+    })
+
+    // 6. Segunda expansão (Largura)
+    .to("#tela-expansiva", {
+      width: 925,
+      duration: 0.8,
+      ease: "power2.inOut"
+    })
+
+    // 7. Revelar o título, texto e botão
+    .to("#conteudo-parabens", {
+      autoAlpha: 1,
+      duration: 0.5,
+      ease: "power1.out",
+      onComplete: () => {
+        // Libera a tela novamente
+        document.body.style.pointerEvents = "auto";
+      }
+    });
+
+  });
 });
